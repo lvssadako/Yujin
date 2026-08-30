19830c48452a91861071d7eb06cfa783d5d28687
shorekeeper <yooh23473@gmail.com>
2026-08-29 21:48:39 -0600

    fix: resolve toproles import path in messageReactionAdd_levels.js

--- src/events/messageReactionAdd_levels.js
+++ src/events/messageReactionAdd_levels.js
@@ -1,7 +1,7 @@
 const logger = require('../utils/logger');
 const { Events } = require('discord.js');
 const { handleLevelRoles } = require('../utils/levelRoles');
-const { updateTopRoles } = require('../commands/toproles');
+const { updateTopRoles } = require('../commands/utility/toproles');
 const { readConfig } = require('../utils/configCache');
 const { readLevels, writeLevels, ensureUserData } = require('../services/level').levelService;
 const { updateMissionProgress } = require('../utils/dailyMissions');
