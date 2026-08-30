1693a435c4a893b8c45075184c417fb2bef83265
shorekeeper <yooh23473@gmail.com>
2026-08-29 21:44:53 -0600

    fix: resolve syntax error caused by escaped newlines in index.js

--- src/index.js
+++ src/index.js
@@ -91,7 +91,16 @@ client.once(Events.ClientReady, async () => {
 
 // Cargar handler de interacciones (¡PROFESIONAL!)
 require('./events/interactionCreate')(client);
-require('./events/messageCreate')(client);\n// Cargar sistemas de Seguridad y Auditoría\ntry {\n  require('./events/messageCreate_automod')(client);\n  require('./events/messageDelete_audit')(client);\n  require('./events/messageUpdate_audit')(client);\n  logger.info('Sistemas Automod y Audit cargados.');\n} catch (e) {\n  logger.error('Error cargando sistemas de seguridad', { error: e.message });\n}
+require('./events/messageCreate')(client);
+// Cargar sistemas de Seguridad y Auditoría
+try {
+  require('./events/messageCreate_automod')(client);
+  require('./events/messageDelete_audit')(client);
+  require('./events/messageUpdate_audit')(client);
+  logger.info('Sistemas Automod y Audit cargados.');
+} catch (e) {
+  logger.error('Error cargando sistemas de seguridad', { error: e.message });
+}
 // Cargar y reprogramar timers de bump pendientes
 require('./events/bumpTimersLoader')(client);
 require('./services/giveaways/giveawayManager').init(client);
